0f90585cfd8aa7e639c9ea00a989df132e362ac1
jameskiprop <jameskiprop01@gmail.com>
2024-07-29 18:08:13 +0300

    progress on DOM project 1

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-// // Creating a new div element
-// var newDiv = document.createElement("div");
-// // Creating a text node
-// var newContent = document.createTextNode("hey how are you doing");
-// // Adding the text node to the newly created div
-// newDiv.appendChild(newContent);
-// // Adding the newly created element and its content into the DOM
-// var latestDiv = document.getElementById("main");
-// document.body.appendChild(newDiv, latestDiv);
+/*Adjust the quantity of each item through  “+” and “-” buttons.*/
+//1. Adding EventListeners
+var plusCircle = document.getElementsByClassName(" fa-plus-circle");
+var minusCircle = document.getElementsByClassName(" fa-minus-circle");
 
-//getting new HTML documents
-var contents = document.getElementById("main").innerHTML;
-console.log("contents", contents);
-//setting inner HTML elements
-var mainDiv = document.getElementById("main");
-mainDiv.innerHTML = "<p>this is newly inserted paragraph</p>";
+for (let i = 0; i < plusCircle.length; i++) {
+  plusCircle[i].addEventListener("click", addItem);
+}
+
+function addItem() {
+  console.log("Okay");
+}
+addItem();
+
+//2. Handling the click events
+plusCircle.addEventListener("click", () => {
+  console.log("item Added");
+});
+
+minusCircle.addEventListener("click", () => {
+  console.log("Item Removed");
+});
+//3. Updating the Display:
+function addItem(a) {}
+
+//4. Recalculating the total price
+
+/*Delete items from the cart.*/
+//1. Adding EventListeners
+//2. Handling the click events
+//3. Recalculating the total price
+
+/*Like items through a clickable heart-shaped button that will change color accordingly.*/
+
+//1. Adding EventListeners
+//2. Handling the click events
+//3. Updating the Display:
+
+/*See the total price adjusted according to quantity and deletions.*/
+
+//1. Recalculate the total price
+//2. Updating the display
